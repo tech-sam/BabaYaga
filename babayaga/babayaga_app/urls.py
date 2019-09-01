456f5eb31b6b5015205ed24e1632ca9a86d661f8
Sumit Sharma <sumitsharma@Sumit-MacBook-Pro.local>
2019-09-01 19:37:20 +0530

    Initial commit for schema dump api

--- babayaga/babayaga_app/urls.py
+++ babayaga/babayaga_app/urls.py
@@ -1,7 +1,12 @@
+from django.urls import path, include
 from rest_framework import routers
-from .api import DbPropsViewSet
+from .views import index, dumpSchema, DbPropsAPI
 
 router = routers.DefaultRouter()
-router.register('api/babayaga',DbPropsViewSet,'babayaga')
+router.register("api/babayaga", DbPropsAPI)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', index, name="index"),
+    path('', include(router.urls)),
+    path('api/dump-schema', dumpSchema, name="dumpSchema"),
+]
--- babayaga/babayaga_app/views.py
+++ babayaga/babayaga_app/views.py
@@ -1,3 +1,52 @@
+import json
+from subprocess import PIPE, Popen
+import shlex
+
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from babayaga_app.models import DatabseServerProps
+from rest_framework import viewsets, permissions
+from .serializers import DbPropsSerialzer
+
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("Success")
+
+
+class DbPropsAPI(viewsets.ModelViewSet):
+    queryset = DatabseServerProps.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = DbPropsSerialzer
+
+
+def dumpSchema(request):
+    try:
+
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        hostName = body['hostName']
+        port = body['port']
+        dbName = body['databaseName']
+        userName = body['userName']
+        password = body['password']
+        schemaName = body['schemaName']
+        pgDump(hostName, port, dbName, userName, password, schemaName)
+        return HttpResponse("Latest Data Fetched from Stack Overflow")
+    except e as Exception:
+        return HttpResponse(f"Failed {e}")
+
+
+def pgDump(host_name, port, database_name, user_name, database_password, schema_name):
+    print("inside pdDump")
+    print(host_name+" "+port)
+    command = 'pg_dump -h {0} -d {1} -U {2} -p {3} -n {4} --file majani-life.sql'\
+        .format(host_name, database_name, user_name, port, schema_name)
+    print(command)
+    p = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    return p.communicate('{}\n'.format(database_password))
