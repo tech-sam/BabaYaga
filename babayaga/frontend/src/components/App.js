cb284e85738e1c7327ed58f440fc85c492c5bf66
Sumit Sharma <sumitsharma@Sumit-MacBook-Pro.local>
2019-09-01 10:26:57 +0530

    create header component and added watcher for react changes

--- babayaga/frontend/src/components/App.js
+++ babayaga/frontend/src/components/App.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
+import Header from './layout/Header';
+
 class App extends Component {
 
     render(){
-      return <h1>BabaYaga bro</h1>  
+      return (
+        <Header/>
+      ) 
     }
 }
 ReactDOM.render(<App />, document.getElementById('app'));
---
+++ babayaga/frontend/src/components/layout/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-sm navbar-light bg-light">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <a className="navbar-brand" href="#">BabaYaga</a>
+
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                    </ul>
+                </div>
+            </nav>
+
+        )
+    }
+}
+
+export default Header
