1492b5f819c2c98fdf9af8c6ac5673002dd1f31b
Sumit Sharma <sumitsharma@Sumit-MacBook-Pro.local>
2019-09-01 10:05:31 +0530

    created react front end app

--- babayaga/babayaga/settings.py
+++ babayaga/babayaga/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'babayaga_app',
-    'rest_framework'
+    'rest_framework',
+    'frontend'
 ]
 
 MIDDLEWARE = [
