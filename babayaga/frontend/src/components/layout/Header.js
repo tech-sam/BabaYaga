ed3507e5a972d33b279a936406ce6e89f55f1fe5
Sumit Sharma <sumitsharma@Sumit-MacBook-Pro.local>
2019-09-02 15:23:18 +0530

    added scss loader for webpack

--- babayaga/frontend/src/components/layout/Header.js
+++ babayaga/frontend/src/components/layout/Header.js
@@ -1,22 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./Header.scss";
 
 export class Header extends Component {
-    render() {
-        return (
-            <nav className="navbar navbar-expand-sm navbar-light bg-light">
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <a className="navbar-brand" href="#">BabaYaga</a>
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm navbar-light bg-light">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarTogglerDemo03"
+          aria-controls="navbarTogglerDemo03"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <a className="navbar-brand" href="#">
+          BabaYaga
+        </a>
 
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                    </ul>
-                </div>
-            </nav>
-
-        )
-    }
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0"></ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
-export default Header
+export default Header;
