02e0c85ae77d9f7112e7ae2d1a830a7f9e43c262
Sourabh <sourabh.siddhu@greytip.com>
2019-09-05 15:07:39 +0530

    Loader added for schema dump

---
+++ babayaga/frontend/src/components/loader/LoaderComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./LoaderComponent.scss"
+function LoaderComponent(props) {
+    return (
+        <div className="lds-hourglass"></div>
+    );
+}
+export default LoaderComponent;
--- babayaga/frontend/src/components/schema-list/SchemaListComponent.js
+++ babayaga/frontend/src/components/schema-list/SchemaListComponent.js
@@ -1,18 +1,33 @@
 import React from "react";
 import SchemaComponent from "../schema/SchemaComponent";
+import LoaderComponent from '../loader/LoaderComponent';
+import "./SchemaListComponent.scss"
+
 function SchemaListComponent(props) {
+    function renderContent() {
+        if (props.isLoading) {
+            return [
+                <div className="loader-wrapper">
+                    <LoaderComponent />
+                </div>
+            ];
+        }
+      
+        return [
+            <div className="database-list">
+                {props.schemaList.map((schema, key) => (
+                    <SchemaComponent key={key} schema={schema}></SchemaComponent>
+                ))}
+            </div>
+        ];
+    }
     return (
         <div>
             <div className="form-group border-bottom border-danger">
                 <legend className="form-header">{props.formName} Schema List</legend>
             </div>
             <div className="container dropable">
-     
-                <div className="database-list">
-                    {props.schemaList.map((schema, key) => (
-                        <SchemaComponent key={key} schema={schema}></SchemaComponent>
-                    ))}
-                </div>
+                {renderContent()}
             </div>
         </div>
     );
